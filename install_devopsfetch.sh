00a6803e4332a3a61c567e208af36bb500d5742e
Adeshile Oluwatosin <105433596+TheTosin@users.noreply.github.com>
2024-07-24 14:38:54 -0700

    install_devopsfetch.sh

--- install_devopsfetch.sh
+++ install_devopsfetch.sh
@@ -10,30 +10,40 @@ function print_header() {
 
 print_header "Installing Dependencies"
 sudo apt-get update
-sudo apt-get install -y docker.io nginx
+sudo apt-get install -y docker.io nginx jq
 
 print_header "Copying devopsfetch Script"
-sudo cp /mnt/c/Users/HP/downloads/devopsfetch/devopsfetch.sh /usr/local/bin/devopsfetch
+sudo cp devopsfetch.sh /usr/local/bin/devopsfetch
 sudo chmod +x /usr/local/bin/devopsfetch
-
 print_header "Creating Systemd Service"
-sudo bash -c 'cat > /etc/systemd/system/devopsfetch.service <<EOF
+bash -c 'cat > /etc/systemd/system/devopsfetch.service <<EOF
 [Unit]
 Description=DevOpsFetch Monitoring Service
 After=network.target
-
 [Service]
-ExecStart=/usr/local/bin/devopsfetch.sh -t "1 hour ago" "now"
+ExecStart=/usr/local/bin/devopsfetch -t "1 hour ago" "now"
 Restart=always
 RestartSec=60
-
 [Install]
 WantedBy=multi-user.target
 EOF'
 
 print_header "Starting and Enabling devopsfetch Service"
-sudo systemctl daemon-reload
-sudo systemctl enable devopsfetch
-sudo systemctl start devopsfetch
+systemctl daemon-reload
+systemctl enable devopsfetch
+systemctl start devopsfetch
+cat <<EOF > /etc/logrotate.d/devopsfetch
+/var/log/devopsfetch.log {
+    daily
+    rotate 7
+    compress
+    missingok
+    notifempty
+    create 0640 root adm
+    postrotate
+        systemctl reload devopsfetch.service > /dev/null 2>/dev/null || true
+    endscript
+}
+EOF
 
 print_header "Installation Completed"
